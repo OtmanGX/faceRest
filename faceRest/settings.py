33088ce7e9a4171e7e4d3f5285eaabcb4fd10178
otmangx <otmangx@gmail.com>
2020-06-03 17:43:05 +0100

    Modify settings

--- faceRest/settings.py
+++ faceRest/settings.py
@@ -161,4 +161,4 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_ROOT = os.path.join(os.path.expanduser("~"), "Public/media")
+MEDIA_ROOT = os.path.join(os.path.expanduser("~"), "facemedia")
